51f0330be305d5c71fae52a8de7ea0dab31de9b7
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-16 00:43:44 +0100

    type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
 from random import uniform
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
     for _ in range(10):
-        yield uniform(0, 10)
         await asyncio.sleep(1)
+        yield uniform(0, 10)
