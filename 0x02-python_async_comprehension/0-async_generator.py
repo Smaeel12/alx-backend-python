1bd6ea69e119ca2baa34047f9cdbb9324443724e
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-16 00:14:08 +0100

    0x02. Python - Async Comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import asyncio
+from random import uniform
+
+
+async def async_generator():
+    for _ in range(10):
+        yield uniform(0, 10)
+        await asyncio.sleep(1)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import asyncio
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    return [i async for i in async_generator()]
