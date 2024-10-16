386626ba1479f355328e09f1c9ee4e4ec4848d08
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-16 01:08:01 +0100

    rounding number

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,5 +9,5 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     """An asynchronous generator"""
     for _ in range(10):
+        yield uniform(0, 10) * 10
         await asyncio.sleep(1)
-        yield uniform(0, 10)
